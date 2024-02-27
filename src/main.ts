aa14df731c28a157f9c72f385ff6674b714c40f2
harry <1577791638@qq.com>
2024-02-27 23:40:23 +0800

    feat: 引入unocss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "virtual:uno.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
