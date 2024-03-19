460142f186fdea990e87f335f34707ceaf638ff0
harry <1577791638@qq.com>
2024-03-19 19:54:53 +0800

    feat: error addEventLIstener;

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@ const route = useRoute();
 onBeforeMount(() => {
   import("./lang-modal.ts" as any);
 });
+
+window.addEventListener("error", function (event) {
+  // 检查是否是特定错误，比如 "Failed to load module script"
+  if (event.message.includes("Failed to load module script")) {
+    // 刷新页面
+    window.location.reload();
+  }
+});
 </script>
 
 <template>
